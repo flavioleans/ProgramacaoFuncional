2203fbb3834561d582eb5ead2779a56a60a498f7
flavioleans <leans.flavio@gmail.com>
2021-08-10 20:31:07 -0300

    Pequenas alterações

--- ProgramacaoFuncional/Program.cs
+++ ProgramacaoFuncional/Program.cs
@@ -15,42 +15,12 @@ namespace ProgramacaoFuncional
 {
     class Program
     {
-        //************proxima aula **************
+        //************aplicação concluida**************
         static void Main(string[] args)
         {
             Menu.Criar();
 
-            //Console.WriteLine("********cliente**************");
-            //Cliente c = new Cliente();
-            //c.Nome = "celtonNovo";
-            //c.Telefone = "1125825825";
-            //c.Cpf = "123456789";
-            //c.Gravar();
-
-            //foreach (Cliente cl in new Cliente().Ler())
-            //{
-            //    Console.WriteLine(cl.Nome);
-            //    Console.WriteLine(cl.Telefone);
-            //    Console.WriteLine(cl.Cpf);
-            //    Console.WriteLine("***************************");
-            //}
-
-            //Console.WriteLine("********Usuario**************");
-            //Usuario u = new Usuario();
-            //u.Nome = "SilvausuarioNovo";
-            //u.Telefone = "1125825825";
-            //u.Cpf = "123456789";
-            //u.Gravar();
-
-            //foreach (Usuario us in new Usuario().Ler())
-            //{
-            //    Console.WriteLine(us.Nome);
-            //    Console.WriteLine(us.Telefone);
-            //    Console.WriteLine(us.Cpf);
-            //    Console.WriteLine("***************************");
-            //}
-
-            //Console.Read();
+            
         }
     }
 }
